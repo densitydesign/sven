d6e9341f34232b2256dfb772363d48bf29699c2f
Flavio Curella <flavio.curella@gmail.com>
2012-08-22 19:44:51 -0500

    Fixes a memory leak by using user session instead of thread-bound variable

--- core/views.py
+++ core/views.py
@@ -4,19 +4,22 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-# local storage of objects, crossview
-_data = {}
 
 def _init( request ):
-	_data[ 'status' ] = "ok"
-	_data[ 'user' ] = request.user.username
+	request.session['status'] = 'ok'
+	request.session['user'] = request.user.username
+
 
 def _throwerror( request, error ):
 	_init( request );
 	return _dropjson( request )
 
 def _dropjson(request):
-	j = json.dumps( _data, ensure_ascii=False )
+	data = {
+		'status': request.session['status'],
+		'user': request.session['user']
+	}
+	j = json.dumps( data, ensure_ascii=False )
 	return HttpResponse( j, mimetype="text/plain" )
 	
 def index(request):
